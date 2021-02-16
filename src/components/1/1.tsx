be1ab8119a7b00e6fe3af1578211535128543878
Полякова Виктория Владимировна <Polyakova.V.Vladi@sberbank.ru>
2021-02-16 15:18:38 +0700

    test

--- src/components/1/1.tsx
+++ src/components/1/1.tsx
@@ -4,6 +4,12 @@ import style from './1.module.css'
 
 export function Component1() {
     return (
-        <div>Component 1</div>
+        <div className={style.wrapper}>
+            <div className={style.content}>
+                <div className={style.title}>1!</div>
+                <div className={style.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam</div>
+            </div>
+        </div>
     )
 }
